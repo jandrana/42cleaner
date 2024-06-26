ab80f23bc02b07ac3d4a87eb4c565bcf93f1a03f
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-06-26 19:23:32 +0200

    Added new basic script in utils for finding cache

---
+++ utils/find_cache.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+min_size="10M"
+
+# Prints a list of potential paths that contain >min_size cache ordered by size
+
+printf "Paths with 'cache' in name ordered by size and with minimum size=$min_size\n"
+find $HOME -type d -iname "cache" -print0 | xargs -0 du -ch -t $min_size | sort -hr
+
+printf "\n\nPaths with '.cache' in name ordered by size and with minimum size=$min_size\n"
+find $HOME -type d -iname ".cache" -print0 | xargs -0 du -ch -t $min_size | sort -hr
