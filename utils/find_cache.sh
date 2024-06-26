dd52bc79cc354987b8c4ad495d5c1ee9343d8a67
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-06-26 20:50:35 +0200

    Updated find_cache script

--- utils/find_cache.sh
+++ utils/find_cache.sh
@@ -4,11 +4,17 @@ min_size="1M"
 
 # Prints a list of potential paths that contain >min_size cache ordered by size
 
-printf "Paths with 'cache' in name ordered by size and with minimum size=$min_size\n"
-find $HOME -type d -iname "cache" -print0 | xargs -0 du -ch -t $min_size | sort -hr
+find_path_with() {
+	local path_with=$1
+	local find_path=$(find $HOME -type d -iname "$path_with" -exec du -sh -t $min_size {} + | sort -hr)
+	if [ -n "$find_path" ]; then
+		printf "Paths with \"$path_with\" in name ordered by size and with minimum size=$min_size\n"
+		printf "$find_path\n\n"
+	else
+		printf "No directories larger than $min_size found containing \"$path_with\" in name.\n\n"
+	fi
+}
 
-printf "\n\nPaths with '.cache' in name ordered by size and with minimum size=$min_size\n"
-find $HOME -type d -iname ".cache" -print0 | xargs -0 du -ch -t $min_size | sort -hr
-
-printf "\n\nPaths with 'temp' in name ordered by size and with minimum size=$min_size\n"
-find $HOME -type d -iname ".cache" -print0 | xargs -0 du -ch -t $min_size | sort -hr
+find_path_with "cache"
+find_path_with ".cache"
+find_path_with "temp"
