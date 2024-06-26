4ef0e2aa10b3209a6469cbb8297083455c08df8f
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-06-26 19:07:32 +0200

    Improved code regarding aliases -install/uninstall

--- install.sh
+++ install.sh
@@ -2,17 +2,20 @@
 
 REPO_DIR=$HOME/42cleaner
 INSTALL_DIR=$HOME/.42cleaner
+INSTALL_DIR_BCK=\$HOME/.42cleaner
 
 # Check if the repository already exists in the home directory
 if [ -d "$REPO_DIR" ]; then
 	read -p "The 42cleaner repository already exists. Do you want to update it? (y/n) " update
 	case $update in
-		[Yy]* ) cd "$REPO_DIR"; git pull; printf "" ;;
-		* ) printf "No changes were made to the repository.\n\n" ;;
+		[Yy]* ) cd "$REPO_DIR"; git pull ;;
+		* ) printf "No changes made to the repository.\n" ;;
 	esac
 else
 	git clone https://github.com/jandrana/42cleaner "$REPO_DIR"
 fi
+
+echo ""
 
 # Navigate to the repository
 cd "$REPO_DIR" || { printf "Failed to navigate to the repository directory. Exiting..."; exit 1; }
@@ -46,49 +49,80 @@ case $SHELL in
 		ALIAS_FILE="$HOME/.zshrc"
 		;;
 	*)
-		printf "Unknown shell. Please add the following alias manually to your shell configuration file:"
+		printf "Unknown shell. Please add the following alias manually to your shell configuration file:\n"
 		printf "alias clean='$HOME/clean.sh'"
 		exit 1
 		;;
 esac
 
-# Check if the alias already exists
-if grep -q "alias clean=" $ALIAS_FILE; then
-	existing_alias=$(grep "alias clean=" $ALIAS_FILE)
-	new_alias="alias clean='$INSTALL_DIR/clean.sh'"
-	if [ "$existing_alias" == "$new_alias" ]; then
-		printf "INFO: The alias 'clean' already exists and is the same as the one being installed. No changes made.\n"
-	else
-		read -p "The alias 'clean' already exists but is different. Do you want to overwrite it? (y/n) " overwrite
+
+# Initialize alias variables
+create_alias=1
+alias_name="clean"
+new_alias="alias $alias_name='$INSTALL_DIR_BCK/clean.sh'"
+cleaner_alias=$(grep "='$INSTALL_DIR_BCK/clean.sh'" $ALIAS_FILE)
+
+# Helper function for managing multiple alias found in $ALIAS_FILE (duplicated aliases)
+manage_duplicate_alias() {
+	printf "WARNING: Found duplicated aliases corresponding to 42cleaner:\n"
+	printf "$cleaner_alias\n\n"
+	read -p "Do you want to delete them and create a single alias (avoiding duplication errors)? (y/n) " rem_duplicate
+	case $rem_duplicate in
+		[Yy]* )
+			echo "$cleaner_alias" | while read -r line; do
+				sed -i "\|$line|d" "$ALIAS_FILE"
+			done
+			printf "INFO: Duplicate aliases deleted\n\n"
+			;;
+		* )
+			printf "INFO: No alias deleted\n\n"
+			;;
+	esac	
+}
+
+# Handles different cases if the alias already exists
+if grep -q "alias $alias_name=" $ALIAS_FILE; then
+	if [ $(grep "='$INSTALL_DIR_BCK/clean.sh'" $ALIAS_FILE | wc -l) -gt 1 ]; then
+		manage_multiple_alias
+	fi
+	if grep -q "$new_alias" $ALIAS_FILE; then
+		printf "INFO: The alias $alias_name for the 42Cleaner already exists. No changes made.\n\n"
+		create_alias=0
+	elif grep -q "alias $alias_name=" $ALIAS_FILE; then
+		read -p "The alias $alias_name exists but is not for the 42Cleaner. Do you want to overwrite it? (y/n) " overwrite
 		case $overwrite in
+			[Yy]* )
+				sed -i "/alias $alias_name=/d" $ALIAS_FILE
+				;;
 			[Nn]* )
 				read -p "Do you want to use another name for the alias? (y/n) " rename
 				case $rename in
 					[Yy]* )
 						read -p "Enter the new alias name: " new_alias_name
-						printf "alias $new_alias_name='$INSTALL_DIR/clean.sh'" >> "$ALIAS_FILE"
-						printf "INFO: New alias '$new_alias_name' created in $ALIAS_FILE for running the clean.sh script"
-						printf "\t alias $new_alias_name='$INSTALL_DIR/clean.sh'\n"
+						alias_name=$new_alias_name
+						new_alias="alias $alias_name='$INSTALL_DIR_BCK/clean.sh'"
 						;;
 					* )
-						printf "INFO: No alias added. You can add it manually later using 'alias clean=$INSTALL_DIR/clean.sh'\n"
+						create_alias=0
+						printf "INFO: No alias added. You can add it manually later using: $new_alias\n\n"
 						;;
 				esac
 				;;
 			* )
-				sed -i '/alias clean=/d' $ALIAS_FILE
-				printf "alias clean='$INSTALL_DIR/clean.sh'" >> "$ALIAS_FILE"
-				printf "INFO: New alias 'clean' created in $ALIAS_FILE for running the clean.sh script"
-				printf "\t alias clean='$INSTALL_DIR/clean.sh'\n"
+				create_alias=0
+				printf "INFO: No alias added. You can add it manually later using $new_alias\n\n"
 				;;
 		esac
 	fi
-else
-	printf "# Alias for clean.sh (42cleaner by Jandrana)" >> "$ALIAS_FILE"
-	printf "alias clean='$INSTALL_DIR/clean.sh'" >> "$ALIAS_FILE"
-	printf "INFO: New alias 'clean' created in $ALIAS_FILE for running the clean.sh script"
-	printf "\t alias clean='$INSTALL_DIR/clean.sh'\n"
 fi
 
-printf "SUCCESS: Installation completed"
+if [ $create_alias -eq 1 ]; then
+	sed -i "/Alias for clean.sh/d" $ALIAS_FILE
+	echo "# Alias for clean.sh (42cleaner by Jandrana)" >> "$ALIAS_FILE"
+	printf "$new_alias\n" >> "$ALIAS_FILE"
+	printf "INFO: New alias '$alias_name' created in $ALIAS_FILE for running the clean.sh script\n"
+	printf "\t $new_alias\n\n"
+fi
+
+printf "SUCCESS: Installation completed\n"
 printf "WARNING: Please restart any open shell sessions for the changes to take effect."
--- uninstall.sh
+++ uninstall.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 INSTALL_DIR=$HOME/.42cleaner
+INSTALL_DIR_BCK=\$HOME/.42cleaner
 
 # Remove the .42cleaner folder from the home directory
 if [ -d "$INSTALL_DIR" ]; then
 	rm -rf $INSTALL_DIR
-	echo -e "SUCCESS: $INSTALL_DIR found and deleted\n"
+	printf "SUCCESS: $INSTALL_DIR found and deleted\n\n"
 else
-	echo -e "WARNING: The folder '.42cleaner/' could not be found in the home directory.\n\t If the folder is located elsewhere or named differently, delete it manually.\n"
+	printf "WARNING: The folder '.42cleaner/' could not be found in the home directory.\n\t If the folder is located elsewhere or named differently, delete it manually.\n\n"
 fi
 
 # Remove the 'clean' alias from the shell configuration
@@ -19,33 +20,30 @@ case $SHELL in
 		ALIAS_FILE="$HOME/.zshrc"
 		;;
 	*)
-		echo -e "Unknown shell. Please remove the following alias manually:"
-		echo -e "alias clean='$INSTALL_DIR/clean.sh'"
+		printf "Unknown shell. Please remove the following alias manually:\n"
+		printf "alias clean='$INSTALL_DIR/clean.sh'\n"
 		exit 1
 		;;
 esac
 
 # Check if the alias exists
-if grep -q "alias clean=" $ALIAS_FILE; then
-	existing_alias=$(grep "alias clean=" $ALIAS_FILE)
-	expected_alias="alias clean='$INSTALL_DIR/clean.sh'"
-	if [ "$existing_alias" == "$expected_alias" ]; then
-		# 'sed' used to remove the lines containing the alias and its comment
-		sed -i '/Alias for clean.sh/d' $ALIAS_FILE
-		sed -i '/alias clean=/d' $ALIAS_FILE
-		echo -e "SUCCESS: Alias 'clean' removed from $ALIAS_FILE file\n"
-	else
-		echo -e "INFO: No alias removed from $ALIAS_FILE"
-		echo -e "\tAn alias 'clean' exists in your configuration file but is not for the clean.sh script."
-		echo -e "\tSuch alias has not been removed, since it is not from the 42cleaner\n"
-	fi
+cleaner_alias="='$INSTALL_DIR_BCK/clean.sh'"
+if grep -q $cleaner_alias $ALIAS_FILE; then
+	printf "Alias(es) from 42Cleaner found:\n"
+	printf "$(grep $cleaner_alias $ALIAS_FILE)\n\n"
+	read -p "Do you want to delete them? (y/n) " rem_alias
+	case $rem_alias in
+		[Yy]* )
+			sed -i "/Alias for clean.sh/d" $ALIAS_FILE
+			echo "$cleaner_alias" | while read -r line; do
+				sed -i "\|$line|d" "$ALIAS_FILE"
+			done
+			printf "SUCCESS: Aliases from 42Cleaner removed from $ALIAS_FILE\n\n"
+			;;
+	esac
 else
-	echo -e "WARNING: No alias was removed from $ALIAS_FILE"
-	echo -e "\tPossible reasons:"
-	echo -e "\t1. The alias 'clean' doesn't exist. No actions needed"
-	echo -e "\t2. An alias for the clean.sh exists but is not named 'clean'"
-	echo -e "\t   Please, remove it manually by editing the $ALIAS_FILE file\n"
+	printf "INFO: No alias from 42cleaner found/deleted in $ALIAS_FILE file\n\n"
 fi
 
-echo -e "SUCCESS: Successfully uninstalled"
-echo -e "WARNING: Please restart any open shell sessions for the changes to take effect."
+printf "SUCCESS: Successfully uninstalled\n"
+printf "WARNING: Please restart any open shell sessions for the changes to take effect.\n"
