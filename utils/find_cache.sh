6c5c5e88ca0879a8d13d94085409a626a2a202c1
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-06-26 20:15:59 +0200

    Updated find_cache.sh script

--- utils/find_cache.sh
+++ utils/find_cache.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-min_size="10M"
+min_size="1M"
 
 # Prints a list of potential paths that contain >min_size cache ordered by size
 
@@ -8,4 +8,7 @@ printf "Paths with 'cache' in name ordered by size and with minimum size=$min_si
 find $HOME -type d -iname "cache" -print0 | xargs -0 du -ch -t $min_size | sort -hr
 
 printf "\n\nPaths with '.cache' in name ordered by size and with minimum size=$min_size\n"
+find $HOME -type d -iname ".cache" -print0 | xargs -0 du -ch -t $min_size | sort -hr
+
+printf "\n\nPaths with 'temp' in name ordered by size and with minimum size=$min_size\n"
 find $HOME -type d -iname ".cache" -print0 | xargs -0 du -ch -t $min_size | sort -hr
